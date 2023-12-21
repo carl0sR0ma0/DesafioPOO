e3fab3de9889946d77c481885610fed8f2276624
carl0sR0ma0 <cvpromao@gmail.com>
2023-12-21 14:28:40 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456", "Modelo 1", "1111111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("4987", "Modelo 2", "222222222222", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
